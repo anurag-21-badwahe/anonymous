0acbe2989ff8ca20c01f2909397d2f72f4fda6e7
anurag <anuragbadwahe@gmail.com>
2024-05-09 09:55:52 +0530

    Created message model and made some changes in user model

---
+++ src/modals/Usermsg.ts
@@ -0,0 +1,65 @@
+import mongoose, { Schema, Document } from 'mongoose';
+
+export interface Message extends Document {
+  content: string;
+  createdAt: Date;
+}
+
+const MessageSchema: Schema<Message> = new mongoose.Schema({
+  content: {
+    type: String,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    required: true,
+    default: Date.now,
+  },
+});
+
+export interface User extends Document {
+  username: string;
+  email: string;
+  password: string;
+  profilePic : string,
+  coverPic : string,
+  isAcceptingMessages: boolean;
+  messages: Message[];
+}
+
+// Updated User schema
+const UserSchema: Schema<User> = new mongoose.Schema({
+  username: {
+    type: String,
+    required: [true, 'Username is required'],
+    trim: true,
+    unique: true,
+  },
+  email: {
+    type: String,
+    required: [true, 'Email is required'],
+    unique: true,
+    match: [/.+\@.+\..+/, 'Please use a valid email address'],
+  },
+  password: {
+    type: String,
+    required: [true, 'Password is required'],
+  },
+  profilePic: {
+      type: String,
+  },
+  coverPic: {
+      type: String,
+  },
+  isAcceptingMessages: {
+    type: Boolean,
+    default: true,
+  },
+  messages: [MessageSchema],
+});
+
+const UserModel =
+  (mongoose.models.User as mongoose.Model<User>) ||
+  mongoose.model<User>('User', UserSchema);
+
+export default UserModel;
